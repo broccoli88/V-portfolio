ae18d554279e635f60afe4146613b16b25796278
broccoli88 <kode.kudaki@gmail.com>
2022-04-03 16:57:49 +0200

    submit cd

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -3,7 +3,8 @@
         <h2 v-scrollAnimation>Contact me</h2>
         <form
             class="contact__form"
-            action="https://formsubmit.co/pawel.jaromin@protonmail.com"
+            @click.prevent="handleSubmit"
+            action="https://formsubmit.co/jaromin.pawel@protonmail.com"
             method="POST"
         >
             <section class="input-container area-name">
@@ -182,15 +183,15 @@ export default {
     },
 
     methods: {
-        // handleSubmit() {
-        //     this.v$.$validate();
-        //     if (!this.v$.$error) {
-        //         this.formSubmittedCorrectly = true;
-        //         setTimeout(() => {
-        //             this.formSubmittedCorrectly = false;
-        //         }, 2000);
-        //     }
-        // },
+        handleSubmit() {
+            this.v$.$validate();
+            if (!this.v$.$error) {
+                this.formSubmittedCorrectly = true;
+                setTimeout(() => {
+                    this.formSubmittedCorrectly = false;
+                }, 2000);
+            }
+        },
 
         outline(e) {
             const parent = e.target.parentElement;
