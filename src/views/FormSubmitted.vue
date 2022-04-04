3f96193cf9a9562d5885297828438bdb7ea5e0b9
broccoli88 <kode.kudaki@gmail.com>
2022-04-04 10:19:52 +0200

    submit page

--- src/views/FormSubmitted.vue
+++ src/views/FormSubmitted.vue
@@ -1,9 +1,11 @@
 <template>
     <div class="submitted">
-        <h1>Your message has been send</h1>
-        <Button @click="ETGoHome">Home</Button>
+        <div class="info">
+            <h1>Your message has been send</h1>
+            <Button class="go-home" @click="ETGoHome">Home</Button>
+        </div>
+        <Footer />
     </div>
-    <Footer />
 </template>
 
 <script>
@@ -20,4 +22,27 @@ export default {
 </script>
 
 <style>
+.submitted {
+    width: 100vw;
+    height: 100vh;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+
+    text-align: center;
+
+    overflow-y: hidden;
+}
+
+.info {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
+
+.go-home {
+    margin: 6rem auto;
+}
 </style>
